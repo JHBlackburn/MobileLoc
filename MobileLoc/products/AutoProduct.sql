7aadf72a71e3c8760e310bea7422495baa3ce3b4
JHBlackburn <JHBlackburn@gmail.com>
2020-11-28 16:05:53 -0500

    merge auto product.

--- MobileLoc/products/AutoProduct.sql
+++ MobileLoc/products/AutoProduct.sql
@@ -1,9 +1,15 @@
 ﻿CREATE TABLE products.[AutoProduct]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	Make varchar(100) NOT NULL,
-	Model varchar(100) NOT NULL,
-	IlcoEntryName varchar(100) NOT NULL,
-	IlcoDetails VARCHAR(500) NOT NULL,
-	PriceUSD DECIMAL(18,6) NULL
+	YearMakeModel NVARCHAR(500) NOT NULL,
+	MakeName varchar(100) NOT NULL,
+	ModelName varchar(100) NOT NULL,
+	[Year] INT NOT NULL,
+	IlcoEntryTitle varchar(100) NOT NULL,
+	IlcoKeyBlanks VARCHAR(1000) NOT NULL,
+	IlcoCodeSeries VARCHAR(1000) NOT NULL,
+	IlcoProgramWith VARCHAR(1000) NOT NULL,
+	IlcoSubstitute VARCHAR(1000) NOT NULL,
+	IlcoNotes VARCHAR(1000) NOT NULL,
+	SetUpKeysPriceUSD DECIMAL(18,6) NULL
 )
