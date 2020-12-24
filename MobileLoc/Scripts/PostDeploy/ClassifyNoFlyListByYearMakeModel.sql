f8e8f31ef79e164d9675ce7ed6b47221df5229eb
John Blackburn <JHBlackburn@gmail.com>
2020-12-24 09:21:17 -0500

    adds script to update the no fly list at year make model grain.

---
+++ MobileLoc/Scripts/PostDeploy/ClassifyNoFlyListByYearMakeModel.sql
@@ -0,0 +1,33 @@
+﻿		DROP TABLE IF EXISTS #tempNoFly
+		SELECT 
+			Id,
+			[Year], 
+			Make, 
+			Model, 
+			KeyBlankDetails, 
+			IsNoFlyList
+
+		INTO #tempNoFly
+		FROM stage.IlcoEntryYearMakeModelKeyBlank mmkb
+
+
+		--Volkswagon after 2009
+		UPDATE kb
+			SET IsNoFlyList = 1
+		FROM #tempNoFly as kb
+		WHERE 
+			kb.Make = 'VOLKSWAGEN'
+			AND YEAR >= 2009
+
+
+			
+		/*************************BEGIN UPDATE TARGET TABLE**********************************/
+		UPDATE  ymmkb
+			SET 
+				ymmkb.IsNoFlyList = temp.IsNoFlyList
+
+		FROM stage.IlcoEntryYearMakeModelKeyBlank ymmkb
+		JOIN #tempNoFly temp
+			ON ymmkb.Id = temp.Id
+				;
+		/*************************END UPDATE TARGET TABLE**********************************/
--- MobileLoc/Scripts/PostDeploy/Script.PostDeploy.sql
+++ MobileLoc/Scripts/PostDeploy/Script.PostDeploy.sql
@@ -1,15 +1,4 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
-
+﻿
 --Ilco Entry grain
 :r .\SeedIlcoEntry.sql
 :r .\ClassifyIlcoEntryKeyBlank.sql
@@ -17,6 +6,8 @@ Post-Deployment Script Template
 --Year Make Model (Key Blank) grain
 :r .\SeedIlcoEntryYearMakeModelKeyBlank.sql
 :r .\ClassifyReflashByYearMakeModel.sql
+:r .\ClassifyNoFlyListByYearMakeModel.sql
+
 :r .\SeedYearMakeModel.sql
 
 --Square Items
